e066fe141d884ae824d3c5f0cad0696a243b1b46
muli c <muli3203@gmail.com>
2019-09-30 13:06:34 +0300

    add groundwork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    NavBarComponent,
+    RepoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
