f67c65fdd734ec687d0e3e19607fb5cd24d2d943
root <root@localhost.localdomain>
2019-09-30 23:19:51 -0400

    adds routes to the routing  component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GitComponent } from './git/git.component';
+import { RepoComponent } from './repo/repo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'git', component: GitComponent},
+  {path: 'repo', component: RepoComponent},
+  {path: '', redirectTo: 'git', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
