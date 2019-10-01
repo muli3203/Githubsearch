a3a5eb0a50f62eb89eb132e3e10847afcfb23b02
root <root@localhost.localdomain>
2019-09-30 23:11:21 -0400

    update the apiKey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '85552a5f912a38b55cd82cff38121c585248ca29'
 };
 
 /*
